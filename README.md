8745d1cde53353e268da7911b18dfcbd9025c8a1
Steve Buzonas <steve@fancyguy.com>
2015-04-06 21:51:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sbuzonas
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sbuzonas'
+maintainer       'Steve Buzonas'
+maintainer_email 'steve@fancyguy.com'
+license          'mit'
+description      'Installs/Configures sbuzonas'
+long_description 'Installs/Configures sbuzonas'
+version          '0.1.0'
+
